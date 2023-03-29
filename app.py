ff4f4484f80b1a8e00e0474006e724da0592d1e9
Rahman Roshan <rahmanroshan01@gmail.com>
2023-03-29 17:09:39 +0530

    added model to print on the console

--- app.py
+++ app.py
@@ -22,8 +22,10 @@ main_df = pd.DataFrame(columns=['model',
                                     'high_margin %'])
 
 for i, row in phone_list.iterrows():
-    website = f"https://uk.webuy.com/search?stext=+{row[0]}&Grade={row[3]}"
-
+    website = f"https://uk.webuy.com/search?stext=+{row[0]} {row[1]}&Grade={row[3]}"
+    print("================================")
+    print(f"{row[0]} {row[1]}")
+    print("================================")
     # Selenium and Chrome webscraping configs
     user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/110.0"
 
